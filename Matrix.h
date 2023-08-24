5094c24960dbc22b2db52f8dc2f416ebde2e95dd
cpp-deep-learning <54460355+cpp-deep-learning@users.noreply.github.com>
2023-08-24 12:50:25 +0800

    support OpenCL C++

--- Matrix.h
+++ Matrix.h
@@ -571,9 +571,9 @@ calculate(S_Matrix* a, S_Matrix* b)
     case operate: {
         S_Matrix* result = Create0(a->x, b->y);
         free(result->v);
-        int option;
+        char option;
         println("please input the operate option:");
-        scanf("%d", &option);
+        scanf("%c", &option);
 
         cl_kernel kernel = clCreateKernel(g0->program, "operate", g0->cl_err);
         cl_mem v0, v1, rlt, set;
@@ -594,7 +594,7 @@ calculate(S_Matrix* a, S_Matrix* b)
             NULL,
             g0->cl_err);
         set = clCreateBuffer(
-            g0->context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(int), &option, g0->cl_err);
+            g0->context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(char), &option, g0->cl_err);
 
         result->v = (float*)clEnqueueMapBuffer(g0->cmd_queue, rlt, CL_TRUE, CL_MAP_WRITE, 0, result->x * result->y * sizeof(float), 0, NULL, NULL, g0->cl_err);
 
@@ -758,15 +758,30 @@ calculate(S_Matrix* a, S_Matrix* b)
 
 // #ifndef __cplusplus
 // }
-#endif
+#else
 
-#ifdef __cplusplus
-
+#include <CL/cl2.hpp>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <new>
+#include <vector>
+
 
 typedef unsigned long long Integer;
 typedef long double Double;
+
+template <typename T>
+void* operator new[](size_t size, T)
+{
+    return size == 0 ? nullptr : std::calloc(size, sizeof(T));
+};
+
+template <typename T>
+void* operator new(size_t size, T)
+{
+    return size == 0 ? nullptr : std::calloc(size, sizeof(T));
+};
 
 class Matrix {
 private:
@@ -804,20 +819,44 @@ public:
         delete[] value;
     }
 
+    template <class C>
+    C size() const
+    {
+        return rows * column;
+    }
+
+    template <class Type>
+    void set(const Type& num);
+
+    template <typename U>
+    void set(const U* data);
+
+    template <class N>
+    N info(bool b) const;
+
+    template <class S>
+    friend S* out(const Matrix& M);
+    // friend decltype(S*) out(const Matrix& M);
+    // friend auto out(const Matrix& M); c++14
+
     void set();
+
     void set(const Integer& x, const Integer& y);
-    void set(const Integer& num);
-    void set(const Double* data);
+
     void info() const;
-    friend void display(const Matrix& M);
+
+    Integer info(void* v) const;
+    void* address() const;
     Matrix operator+(const Matrix& M);
     Matrix operator-(const Matrix& M);
     Matrix operator*(const Matrix& M);
     Matrix& operator=(const Matrix& M);
+    bool operator==(const Matrix& M);
     Double& operator()(Integer x0, Integer y0);
+    friend void display(const Matrix& M);
+    friend void* address(const Matrix& M);
     friend Matrix T(const Matrix& M);
     friend Matrix& Eye(const Integer& num);
-    friend Double* M_out(const Matrix& M);
     friend std::ostream& operator<<(std::ostream& os, const Matrix& M);
     friend std::istream& operator>>(std::istream& is, Matrix& M);
     friend Matrix kronecker(const Matrix& A, const Matrix& B);
@@ -832,7 +871,8 @@ void Matrix::set()
     }
 }
 
-void Matrix::set(const Integer& num)
+template <class Type>
+void Matrix::set(const Type& num)
 {
     for (Integer i = 0; i < rows * column; i++)
         value[i] = num;
@@ -845,16 +885,28 @@ void Matrix::set(const Integer& x, const Integer& y)
     value = new Double[x * y];
 }
 
+template <typename U>
+void Matrix::set(const U* data)
+{
+    for (Integer i = 0; i < rows * column; i++)
+        value[i] = data[i];
+}
+
 void Matrix::info() const
 {
     std::cout << "the row is:" << this->rows << "\n"
               << "the column is:" << this->column << std::endl;
 }
 
-void Matrix::set(const Double* data)
+Integer Matrix::info(void* v) const
 {
-    for (Integer i = 0; i < rows * column; i++)
-        value[i] = data[i];
+    return v == nullptr ? rows : column;
+}
+
+template <class N>
+N Matrix::info(bool b) const
+{
+    return N(b == false ? rows : column);
 }
 
 void display(const Matrix& M)
@@ -866,6 +918,385 @@ void display(const Matrix& M)
                       << std::endl;
     }
 }
+
+void* Matrix::address() const
+{
+    return (void*)this;
+}
+
+void* address(const Matrix& M)
+{
+    return M.value == nullptr ? nullptr : M.value;
+}
+
+Matrix
+T(const Matrix& M)
+{
+    Integer x, y, i;
+    if (M.rows != 0 && M.column != 0) {
+        Matrix* target = new Matrix(M.column, M.rows);
+        for (i = 0; i < M.rows * M.column; i++) {
+            x = i / M.column;
+            y = i - M.column * x;
+            target->value[y * M.rows + x] = M.value[x * M.column + y];
+        }
+        return *target;
+    } else {
+        return Matrix(0, 0);
+    }
+}
+
+Double&
+Matrix::operator()(Integer x0, Integer y0)
+{
+    if (x0 > rows || y0 > column)
+        std::cout << "error" << std::endl;
+    else
+        std::cout << "the Matrix[" << x0 << "]"
+                  << "[" << y0 << "]is:" << value[(x0 - 1) * column + (y0 - 1)]
+                  << std::endl;
+    return value[(x0 - 1) * column + (y0 - 1)];
+}
+
+Matrix&
+Matrix::operator=(const Matrix& M)
+{
+    this->rows = M.rows;
+    this->column = M.column;
+    this->value = M.value;
+    return *this;
+}
+
+bool Matrix::operator==(const Matrix& M)
+{
+    if (M.rows != rows || M.column != column)
+        return false;
+    else {
+        for (int i = 0; i < rows * column; i++) {
+            if (M.value[i] == value[i])
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+}
+
+std::ostream&
+operator<<(std::ostream& os, const Matrix& M)
+{
+    for (Integer i = 0; i < M.rows * M.column; i++) {
+        os << M.value[i] << "    ";
+        if ((i + 1) % M.column == 0)
+            os << "\n"
+               << std::endl;
+    }
+    return os;
+}
+
+std::istream&
+operator>>(std::istream& is, Matrix& M)
+{
+    for (Integer i = 0; i < M.rows * M.column; i++) {
+        std::cout << "please input the Matrix[" << i / M.column + 1 << "]"
+                  << "[" << i - (i / M.column) * M.column + 1 << "]" << std::endl;
+        std::cin >> M.value[i];
+    }
+    return is;
+}
+
+template <class S>
+S* out(const Matrix& M)
+{
+    if (M.value != nullptr) {
+        S* data = new S[M.rows * M.column];
+        for (Integer i = 0; i < M.rows * M.column; i++)
+            data[i] = M.value[i];
+        return data;
+    } else {
+        std::cout << "copy failed!" << std::endl;
+        return nullptr;
+    }
+}
+
+Matrix
+kronecker(const Matrix& A, const Matrix& B)
+{
+    Integer i, j, k, l;
+    if (A.rows <= 0 || A.column <= 0 || B.rows <= 0 || B.column <= 0 || A.value == nullptr || B.value == nullptr) {
+        std::cout << "Generating Error!!!" << std::endl;
+        return Matrix(0, 0);
+    } else {
+        Matrix* result = new Matrix(A.rows * B.rows, A.column * B.column);
+        for (l = 0; l < A.rows; l++) {
+            for (k = 0; k < B.rows; k++) {
+                for (j = 0; j < A.column; j++) {
+                    for (i = 0; i < B.column; i++)
+                        result->value[(l * B.column + k) * A.column * B.column + j * B.column + i] = A.value[l * A.column + j] * B.value[k * B.column + i];
+                }
+            }
+        }
+        return *result;
+    }
+}
+
+// support OpenCL C++
+
+inline std::string f_to_string(const std::string& path)
+{
+    std::ifstream input(path);
+    std::string Content, tmp;
+    if (input.is_open()) {
+        while (std::getline(input, tmp)) {
+            Content += tmp + "\n";
+        }
+        input.close();
+        std::cout << "read success!!" << std::endl;
+    } else {
+        std::cout << "read err!!" << std::endl;
+    }
+    return Content;
+}
+
+inline bool checkExtensionSupport(const cl::Device& device, const std::string& extensionName)
+{
+    std::string extensions = device.getInfo<CL_DEVICE_EXTENSIONS>();
+    return (extensions.find(extensionName) != std::string::npos);
+}
+
+inline cl::Program build_program(const cl::Context& context, const cl::Device& device, const std::string& sourceFile)
+{
+    cl::Program program = cl::Program(context, f_to_string(sourceFile));
+    std::string options = "-cl-std=CL2.0";
+    if (checkExtensionSupport(device, "cl_khr_fp64")) 
+        options += " -DENABLE_DOUBLE_PRECISION";
+    cl_int err = program.build({ device }, options.c_str());
+    if (err != CL_SUCCESS) 
+        std::cerr << "Failed to build the program: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
+
+    return program;
+}
+
+inline void check(int input, const std::string& info)
+{
+    if (input != CL_SUCCESS)
+        std::cout << info << "error" << std::endl;
+}
+
+class cl_group {
+public:
+    std::vector<cl::Platform> platforms;
+    std::vector<cl::Device> devices;
+    cl::Platform platform;
+    cl::Device device;
+    cl::Context context;
+    cl::CommandQueue cmdqueue;
+    cl::Program program;
+    cl_int* clerr;
+    cl_group(const std::string& path)
+    {
+        clerr = new cl_int;
+        cl::Platform::get(&platforms);
+        platform = platforms[0];
+
+        platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+        device = devices[0];
+
+        context = cl::Context(device, nullptr, nullptr, nullptr, clerr);
+        check(*clerr, "context make");
+
+        cmdqueue = cl::CommandQueue(context, device, 0, clerr);
+        check(*clerr, "cmdqueue build");
+
+        program = build_program(context, device, path);
+    }
+    ~cl_group()
+    {
+        delete clerr;
+    }
+};
+
+Matrix calculate(const Matrix& A = *new Matrix(0, 0), const Matrix& B = *new Matrix(0, 0), char choice = -1, char opt = -1, float target = 0)
+{
+    cl_group g0("M.cl");
+
+    if (choice == -1)
+        std::cin >> choice;
+    switch (choice) {
+    case 48: {
+        if (opt == -1)
+            std::cin >> opt;
+
+        cl::Kernel kernel(g0.program, "operate", g0.clerr);
+        check(*g0.clerr, "kernel make");
+
+        cl::Buffer a, b, c, d;
+
+        a = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(float) * A.size<size_t>(), out<float>(A), g0.clerr);
+        check(*g0.clerr, "buffer create");
+        b = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(float) * B.size<size_t>(), out<float>(B), g0.clerr);
+        check(*g0.clerr, "buffer create");
+        c = cl::Buffer(g0.context, CL_MEM_READ_WRITE, sizeof(float) * A.info<size_t>(0) * B.info<size_t>(1), nullptr, g0.clerr);
+        check(*g0.clerr, "buffer create");
+        d = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(char), &opt, g0.clerr);
+        check(*g0.clerr, "buffer create");
+
+        kernel.setArg(0, a);
+        kernel.setArg(1, b);
+        kernel.setArg(2, c);
+        kernel.setArg(3, d);
+
+        // g0.cmdqueue.finish();
+
+        *g0.clerr = g0.cmdqueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(A.size<size_t>()), cl::NDRange(1), nullptr, nullptr);
+        check(*g0.clerr, "kernel execute");
+
+        float* data = (float*)g0.cmdqueue.enqueueMapBuffer(c, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE, 0, sizeof(float) * A.size<size_t>(), nullptr, nullptr, g0.clerr);
+        // g0.cmdqueue.enqueueReadBuffer(c,CL_TRUE,0,A.size<size_t>()*sizeof(float),data,nullptr,nullptr);
+
+        g0.cmdqueue.enqueueUnmapMemObject(c, data, nullptr, nullptr);
+
+        Matrix M(A.info<Integer>(0), A.info<Integer>(1));
+        M.set<float>(data);
+
+        return (A.info(nullptr) == B.info(nullptr) && A.info(address(M)) == B.info(address(M))) ? M : Matrix(0, 0);
+        break;
+    }
+    case 49: {
+        long H[2] = { A.info<long>(1), B.info<long>(1) };
+
+        cl::Kernel kernel(g0.program, "multiple", g0.clerr);
+
+        cl::Buffer X, Y, O, Z;
+
+        X = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(float) * A.size<size_t>(), out<float>(A), g0.clerr);
+        check(*g0.clerr, "buffer create");
+        Y = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(float) * B.size<size_t>(), out<float>(B), g0.clerr);
+        check(*g0.clerr, "buffer create");
+        Z = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(long) * 2, H, g0.clerr);
+        check(*g0.clerr, "buffer create");
+        O = cl::Buffer(g0.context, CL_MEM_READ_WRITE, sizeof(float) * A.info<size_t>(0) * B.info<size_t>(1), nullptr, g0.clerr);
+        check(*g0.clerr, "buffer create");
+
+        kernel.setArg(0, X);
+        kernel.setArg(1, Y);
+        kernel.setArg(2, O);
+        kernel.setArg(3, Z);
+
+        *g0.clerr = g0.cmdqueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(A.info<size_t>(0), B.info<size_t>(1)), cl::NDRange(1, B.info<size_t>(1)), nullptr, nullptr);
+        check(*g0.clerr, "kernel execute");
+
+        float* data = (float*)g0.cmdqueue.enqueueMapBuffer(O, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE, 0, sizeof(float) * A.info<size_t>(0) * B.info<size_t>(1), nullptr, nullptr, g0.clerr);
+        check(*g0.clerr, "data Map");
+
+        *g0.clerr = g0.cmdqueue.enqueueUnmapMemObject(O, data, nullptr, nullptr);
+        check(*g0.clerr, "unmap data");
+
+        Matrix M(A.info<Integer>(0), B.info<Integer>(1));
+        M.set<float>(data);
+
+        return (A.info(address(M)) == B.info(nullptr)) ? M : Matrix(0, 0);
+        break;
+    }
+    case 50: {
+        unsigned long info[2] = { A.info<unsigned long>(0), A.info<unsigned long>(1) };
+
+        cl::Kernel kernel(g0.program, "eye", g0.clerr);
+
+        cl::Buffer x, y;
+
+        x = cl::Buffer(g0.context, CL_MEM_READ_WRITE, sizeof(float) * A.size<size_t>(), nullptr, g0.clerr);
+        check(*g0.clerr, "buffer create");
+        y = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(unsigned long) * 2, info, g0.clerr);
+        check(*g0.clerr, "buffer create");
+
+        kernel.setArg(0, x);
+        kernel.setArg(1, y);
+
+        *g0.clerr = g0.cmdqueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(A.info<size_t>(0), A.info<size_t>(1)), cl::NDRange(1, 1), nullptr, nullptr);
+        check(*g0.clerr, "kernel execute");
+
+        float* data = (float*)g0.cmdqueue.enqueueMapBuffer(x, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE, 0, sizeof(float) * A.info<size_t>(0) * B.info<size_t>(1), nullptr, nullptr, g0.clerr);
+        check(*g0.clerr, "data Map");
+
+        *g0.clerr = g0.cmdqueue.enqueueUnmapMemObject(x, data, nullptr, nullptr);
+        check(*g0.clerr, "unmap data");
+
+        Matrix M(A);
+        M.set<float>(data);
+
+        return (A.info(nullptr) == A.info(address(A))) ? M : Matrix(0, 0);
+        break;
+    }
+    case 51: {
+        cl::Kernel kernel(g0.program, "e", g0.clerr);
+
+        cl::Buffer x, y;
+
+        x = cl::Buffer(g0.context, CL_MEM_READ_WRITE, sizeof(float) * A.size<size_t>(), nullptr, g0.clerr);
+        check(*g0.clerr, "buffer create");
+        y = cl::Buffer(g0.context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(float), &target, g0.clerr);
+        check(*g0.clerr, "buffer create");
+
+        kernel.setArg(0, x);
+        kernel.setArg(1, y);
+
+        *g0.clerr = g0.cmdqueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(A.size<size_t>()), cl::NDRange(1), nullptr, nullptr);
+        check(*g0.clerr, "kernel execute");
+
+        float* data = (float*)g0.cmdqueue.enqueueMapBuffer(x, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE, 0, sizeof(float) * A.info<size_t>(0) * B.info<size_t>(1), nullptr, nullptr, g0.clerr);
+        check(*g0.clerr, "data Map");
+
+        *g0.clerr = g0.cmdqueue.enqueueUnmapMemObject(x, data, nullptr, nullptr);
+        check(*g0.clerr, "unmap data");
+
+        Matrix M(A.info(nullptr), A.info(data));
+        M.set<float>(data);
+        return M;
+        break;
+    }
+    default: {
+        return Matrix(0, 0);
+        break;
+        }
+    }
+}
+
+inline Matrix calculate(Matrix& M, char choices)
+{
+    return calculate(M, Matrix(0, 0), choices);
+}
+
+inline Matrix calculate(Matrix& M, char choices, float F)
+{
+    return calculate(M, Matrix(0, 0), choices, -1, F);
+}
+
+#ifdef OPERATE_RELOAD_OPENCL
+
+Matrix Matrix::operator*(const Matrix& B)
+{
+    return calculate(*this, B, 49);
+}
+
+Matrix Matrix::operator+(const Matrix& B)
+{
+    return calculate(*this, B, 48, 48);
+}
+
+Matrix Matrix::operator-(const Matrix& B)
+{
+    return calculate(*this, B, 48, 49);
+}
+
+Matrix& Eye(const Integer I)
+{
+    Matrix A(I, I);
+    A = calculate(A, 50);
+    return A;
+}
+
+#else
 
 Matrix
 Matrix::operator+(const Matrix& M)
@@ -900,35 +1331,6 @@ Matrix::operator-(const Matrix& M)
 }
 
 Matrix
-T(const Matrix& M)
-{
-    Integer x, y, i;
-    if (M.rows != 0 && M.column != 0) {
-        Matrix* target = new Matrix(M.column, M.rows);
-        for (i = 0; i < M.rows * M.column; i++) {
-            x = i / M.column;
-            y = i - M.column * x;
-            target->value[y * M.rows + x] = M.value[x * M.column + y];
-        }
-        return *target;
-    } else {
-        return Matrix(0, 0);
-    }
-}
-
-Double&
-Matrix::operator()(Integer x0, Integer y0)
-{
-    if (x0 > rows || y0 > column)
-        std::cout << "error" << std::endl;
-    else
-        std::cout << "the Matrix[" << x0 << "]"
-                  << "[" << y0 << "]is:" << value[(x0 - 1) * column + (y0 - 1)]
-                  << std::endl;
-    return value[(x0 - 1) * column + (y0 - 1)];
-}
-
-Matrix
 Matrix::operator*(const Matrix& M)
 {
     Integer i, j, k, n;
@@ -951,15 +1353,6 @@ Matrix::operator*(const Matrix& M)
 }
 
 Matrix&
-Matrix::operator=(const Matrix& M)
-{
-    this->rows = M.rows;
-    this->column = M.column;
-    this->value = M.value;
-    return *this;
-}
-
-Matrix&
 Eye(const Integer& num)
 {
     Matrix* m0 = new Matrix(num);
@@ -968,62 +1361,6 @@ Eye(const Integer& num)
     return *m0;
 }
 
-std::ostream&
-operator<<(std::ostream& os, const Matrix& M)
-{
-    for (Integer i = 0; i < M.rows * M.column; i++) {
-        os << M.value[i] << "    ";
-        if ((i + 1) % M.column == 0)
-            os << "\n"
-               << std::endl;
-    }
-    return os;
-}
-
-std::istream&
-operator>>(std::istream& is, Matrix& M)
-{
-    for (Integer i = 0; i < M.rows * M.column; i++) {
-        std::cout << "please input the Matrix[" << i / M.column + 1 << "]"
-                  << "[" << i - (i / M.column) * M.column + 1 << "]" << std::endl;
-        std::cin >> M.value[i];
-    }
-    return is;
-}
-
-Double*
-M_out(const Matrix& M)
-{
-    if (M.value != nullptr) {
-        Double* data = new Double[M.rows * M.column];
-        for (Integer i = 0; i < M.rows * M.column; i++)
-            data[i] = M.value[i];
-        return data;
-    } else {
-        std::cout << "copy failed!" << std::endl;
-        return nullptr;
-    }
-}
-
-Matrix
-kronecker(const Matrix& A, const Matrix& B)
-{
-    Integer i, j, k, l;
-    if (A.rows <= 0 || A.column <= 0 || B.rows <= 0 || B.column <= 0 || A.value == nullptr || B.value == nullptr) {
-        std::cout << "Generating Error!!!" << std::endl;
-        return Matrix(0, 0);
-    } else {
-        Matrix* result = new Matrix(A.rows * B.rows, A.column * B.column);
-        for (l = 0; l < A.rows; l++) {
-            for (k = 0; k < B.rows; k++) {
-                for (j = 0; j < A.column; j++) {
-                    for (i = 0; i < B.column; i++)
-                        result->value[(l * B.column + k) * A.column * B.column + j * B.column + i] = A.value[l * A.column + j] * B.value[k * B.column + i];
-                }
-            }
-        }
-        return *result;
-    }
-}
+#endif
 
 #endif
